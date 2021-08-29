eb488b3200056db955e597e416deb8c38da6e276
Yan Ru <tanyanru97@hotmail.com>
2021-08-29 18:03:51 +0800

    Level 1 - Create lines, greet, echo, exit.

---
+++ src/main/java/Border.java
@@ -0,0 +1,11 @@
+public class Border {
+    public String createLine()
+    {
+        String lines = "";
+        for (int a = 0;  a < 50; a ++)
+        {
+            lines += "-";
+        }
+        return lines;
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo);*/
+
+        Scanner sc = new Scanner(System.in);
+        Border lines = new Border();
+        System.out.println(lines.createLine() + "\n");
+        String input = "Hello! I'm Duke\nWhat can I do for you?";
+        while (!input.equals("bye")){
+            System.out.println(String.format("%45s",input));
+            System.out.println(lines.createLine() + "\n");
+            input = sc.nextLine();
+            System.out.println(lines.createLine() + "\n");
+        }
+        System.out.println(String.format("%50s","Bye. Hope to see you again soon!"));
     }
 }
+
