6f2e0b9d1e5792b7dd500ed04017697ecb3a9f60
Yan Ru <tanyanru97@hotmail.com>
2021-10-07 18:01:16 +0800

    Level 6 - Use Enums

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -7,107 +7,92 @@ public class TaskList
     int taskNum = 0;
     Border lines = new Border();
 
-    public void addTask(String task)
-    {
+    public void addTask(String task) {
         String[] arr = task.split(" ", 2);
-        switch (arr[0])
-        {
-            case "list":
-                System.out.println(toString());
-                break;
 
-            case "done":
-                try
-                {
-                    try
-                    {
-                        if (arr.length < 2)
-                            throw new DukeException((lines.createLine()) + "\n Which tasks have u completed? \n" + (lines.createLine()));
-                        taskList.get(Integer.parseInt(arr[1]) - 1).markAsDone();
-                        System.out.println(lines.createLine());
-                        System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(taskList.get(Integer.parseInt(arr[1]) - 1).toString());
-                        System.out.println(lines.createLine());
-                    } catch (NullPointerException | IndexOutOfBoundsException e)
-                    {
-                        throw new DukeException((lines.createLine()) + "\n Index out of bounds. \n" + (lines.createLine()));
-                    }
-                    catch(NumberFormatException e)
-                    {
-                        throw new DukeException((lines.createLine()) + "\n Please enter the task number that you would like to delete.  \n" + (lines.createLine()));
-                    }
-                } catch (DukeException e)
-                {
-                    System.out.println(e.getMessage());
-                } finally
-                {
+        try {
+            Activity activity = Activity.getByAction(arr[0]);
+            switch (activity) {
+                case LIST:
+                    System.out.println(this);
                     break;
-                }
 
-            case "delete":
-                try {
-                    try{
-                        if (arr.length < 2)
-                            throw new DukeException((lines.createLine()) + "\n Which tasks have u completed? \n" + (lines.createLine()));
-                        Task removeTask = taskList.get(Integer.parseInt(arr[1])-1);
-                        taskList.remove(removeTask);
-                        taskNum --;
-                        System.out.println(lines.createLine());
-                        System.out.println("Noted. I've removed this task: \n" + removeTask.toString());
-                        System.out.println("Now you have " + taskNum + " in the list.");
-                        System.out.println(lines.createLine());
-                    } catch (NullPointerException | IndexOutOfBoundsException e)
-                    {
-                        throw new DukeException (lines.createLine() + "\n Index out of bounds. \n" + (lines.createLine()));
+                case DONE:
+                    try {
+                        try {
+                            if (arr.length < 2)
+                                throw new DukeException((lines.createLine()) + "\n Which tasks have u completed? \n" + (lines.createLine()));
+                            taskList.get(Integer.parseInt(arr[1]) - 1).markAsDone();
+                            System.out.println(lines.createLine());
+                            System.out.println("Nice! I've marked this task as done:");
+                            System.out.println(taskList.get(Integer.parseInt(arr[1]) - 1).toString());
+                            System.out.println(lines.createLine());
+                        } catch (NullPointerException | IndexOutOfBoundsException e) {
+                            throw new DukeException((lines.createLine()) + "\n Index out of bounds. \n" + (lines.createLine()));
+                        } catch (NumberFormatException e) {
+                            throw new DukeException((lines.createLine()) + "\n Please enter the task number that you have completed.  \n" + (lines.createLine()));
+                        }
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                    } finally {
+                        break;
                     }
-                    catch(NumberFormatException e)
-                    {
-                        throw new DukeException((lines.createLine()) + "\n Please enter the task number that you would like to delete.  \n" + (lines.createLine()));
+                case DELETE:
+                    try {
+                        try {
+                            if (arr.length < 2)
+                                throw new DukeException((lines.createLine()) + "\n Which tasks have u completed? \n" + (lines.createLine()));
+                            Task removeTask = taskList.get(Integer.parseInt(arr[1]) - 1);
+                            taskList.remove(removeTask);
+                            taskNum--;
+                            System.out.println(lines.createLine());
+                            System.out.println("Noted. I've removed this task: \n" + removeTask.toString());
+                            System.out.println("Now you have " + taskNum + " in the list.");
+                            System.out.println(lines.createLine());
+                        } catch (NullPointerException | IndexOutOfBoundsException e) {
+                            throw new DukeException(lines.createLine() + "\n Index out of bounds. \n" + (lines.createLine()));
+                        } catch (NumberFormatException e) {
+                            throw new DukeException((lines.createLine()) + "\n Please enter the task number that you would like to delete.  \n" + (lines.createLine()));
+                        }
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                    } finally {
+                        break;
                     }
-                } catch (DukeException e)
-            {
-                System.out.println(e.getMessage());
-            } finally
-            {
-                break;
-            }
 
-        default:
-                try {
-                    Task newTask = track(arr);
-                    taskList.add(newTask);
-                    taskNum ++;
-                    System.out.println(lines.createLine());
-                    System.out.println("Got it. I've added this task: " + "\n" + newTask.toString());
-                    System.out.println("Now you have " + taskNum + " task(s) in the list." + "\n" + lines.createLine());
-                    break;
+                default:
+                    try {
+                        Task newTask = track(activity, arr);
+                        taskList.add(newTask);
+                        taskNum++;
+                        System.out.println(lines.createLine());
+                        System.out.println("Got it. I've added this task: " + "\n" + newTask.toString());
+                        System.out.println("Now you have " + taskNum + " task(s) in the list." + "\n" + lines.createLine());
+                        break;
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
                     }
-                catch (DukeException e)
-                {
-                    System.out.println(e.getMessage());
                 }
+            }  catch (DukeException e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    private Task track(String[] arr) throws DukeException
+
+    private Task track(Activity activity, String[] arr) throws DukeException
     {
         try
         {
-            if (arr[0].equals("todo"))
-            {
-                checkDescription(arr, "todo");
-                return new Todo(arr[1]);
-            } else if (arr[0].equals("deadline"))
-            {
-                checkDescription(arr, "deadline");
-                return new Deadline(arr[1]);
-            } else if (arr[0].equals("event"))
-            {
-                checkDescription(arr, "event");
-                return new Event(arr[1]);
-            } else
-            {
-                throw new DukeException((lines.createLine()) + "\nI'm sorry, but I don't know what that means.\n" + (lines.createLine()), new IllegalArgumentException());
+            switch (activity) {
+                case TODO:
+                    checkDescription(arr, "todo");
+                    return new Todo(arr[1]);
+                case DEADLINE:
+                    checkDescription(arr, "deadline");
+                    return new Deadline(arr[1]);
+                default:
+                    checkDescription(arr, "event");
+                    return new Event(arr[1]);
             }
         }
         catch(DukeException e)
